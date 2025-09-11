6cc485bd8a905856c52fefd1a64f56d6431edf43
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-09-11 20:49:51 +0700

    feat: :sparkles: auth with clerk + webhooks setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  allowedDevOrigins: [
+    'lurlene-chondriosomal-arnulfo.ngrok-free.app',
+    '*.ngrok-free.app',
+    '*.ngrok.io',
+    'localhost',
+    '127.0.0.1'
+  ]
+}
 
-export default nextConfig;
+export default nextConfig
